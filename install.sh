6892882b86923292cb73fa5fee9a2bcc20cb305e
khademianx <124785308+khademianx@users.noreply.github.com>
2025-05-05 14:31:04 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+docker info > /dev/null 2>&1
+
+# Ensure that Docker is running...
+if [ $? -ne 0 ]; then
+    echo "Docker is not running."
+
+    curl -fsSL https://get.docker.com | sh
+fi
+
+mkdir /opt/wireguard && \
+    cd /opt/wireguard && \
+    curl -o ./docker-compose.yml https://raw.githubusercontent.com/khademianx/relay/refs/heads/main/docker-compose.yml && \
+    curl -o ./.env https://raw.githubusercontent.com/wireguard-panel/wireguard-panel/refs/heads/main/.env.example
+
+docker run --rm --entrypoint="" -v /opt/wireguard/.env:/app/.env ahmadrezaei/wireguard-panel sh -c "php artisan key:generate"
+
+docker run --rm -it --entrypoint="" -v /opt/wireguard/.env:/app/.env ahmadrezaei/wireguard-panel:develop sh -c "php artisan setup"
+
+cd /opt/wireguard && docker compose up -d
